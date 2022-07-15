055041aa79f5a9960b41f8f0f0a49d6d44291fa2
ayeshamukhtar007 <81950207+ayeshamukhtar007@users.noreply.github.com>
2022-07-15 21:48:35 +0500

    update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express= require("express");
 var app= express();
-app.set("json space",5);
-dotenv.config({path:"./.env"})
+
 let response={
     framework:"express.js",
     langauges:"javascript"
@@ -15,7 +14,4 @@ app.get("/data",(req,res)=>{
 
     response.message= [1,2,3,4,5,6];
     res.status(200).json(response);
-})
-app.listen(process.env.PORT || 1000,()=>{
-    console.log("port 1000")
 })
